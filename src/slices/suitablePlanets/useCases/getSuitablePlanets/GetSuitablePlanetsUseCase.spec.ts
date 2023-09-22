299489bf31ab3a9131759c5a1232d768cc462a51
joao pedro <jjoao.monteiro15@gmail.com>
2023-09-22 20:16:45 -0300

    test: testing useCase getSuitablePlanets

---
+++ src/slices/suitablePlanets/useCases/getSuitablePlanets/GetSuitablePlanetsUseCase.spec.ts
@@ -0,0 +1,31 @@
+import { MockProxy, mock } from 'jest-mock-extended'
+import { GetSuitablePlanetsRepository } from '../../repositories/contracts'
+import { GetSuitablePlanetsUseCase } from './GetSuitablePlanetsUseCase'
+import { fakePlanetInAPI } from '../../entities/SuitablePlanetsEntity.spec'
+
+describe("testing useCase getSuitablePlanets", () => {
+  let suitablePlanetsRepository: MockProxy<GetSuitablePlanetsRepository>
+  let testInstance: GetSuitablePlanetsUseCase
+
+  beforeAll(() => {
+    suitablePlanetsRepository = mock()
+    suitablePlanetsRepository.get.mockResolvedValue([fakePlanetInAPI, fakePlanetInAPI])
+  })
+
+  beforeEach(() => {
+    testInstance = new GetSuitablePlanetsUseCase(suitablePlanetsRepository)
+  })
+
+  it("should return Not Found Planets error", async () => {
+    suitablePlanetsRepository.get.mockResolvedValueOnce([])
+    await expect(testInstance.execute())
+    .rejects.toThrowError('Not Found Planets')
+  })
+
+  it("should return Not Found Planets error", async () => {
+    const result = await testInstance.execute()
+    expect(suitablePlanetsRepository.get).toHaveBeenCalled()
+    expect(result).toHaveLength(2)
+    expect(result[0]).toEqual({ name: "11 Com b", mass: 0, hasStation: false })
+  })
+})
