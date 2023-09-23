5ab33407c905b9559055f084abf7463c53b87349
joao pedro <jjoao.monteiro15@gmail.com>
2023-09-23 02:15:42 -0300

    chore: change name in station repository

--- src/slices/stations/repositories/StationsRepository.ts
+++ src/slices/stations/repositories/StationsRepository.ts
@@ -1,6 +1,6 @@
 import { AddStationsRepository } from "./contracts";
 
-export class StatiosRepository implements AddStationsRepository {
+export class StationsRepository implements AddStationsRepository {
   constructor(private repository: AddStationsRepository) {}
 
   async add(data: { name: string; planetId: string; }): Promise<any> {
