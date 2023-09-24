cac6f56226e4e541a038df4939155d05e8fdab44
joao pedro <jjoao.monteiro15@gmail.com>
2023-09-24 17:00:44 -0300

    test: testing entity history recharges

---
+++ src/slices/recharges/entities/RechargeHistoryEntity.spec.ts
@@ -0,0 +1,23 @@
+import { IRercharge } from "../../stations/repositories/types"
+import { randomUUID } from 'node:crypto'
+import { RechargeHistoryEntity } from "./RechargeHistoryEntity"
+
+const fakeRecharge: IRercharge = {
+  endDate: new Date('2023-09-30 08:10:00'),
+  id: randomUUID(),
+  startDate: new Date('2023-09-30 08:00:00'),
+  stationId: randomUUID(),
+  user: { id: randomUUID(), name: 'Josézin' },
+  userId: randomUUID()
+}
+
+describe('testing entity RechargeHistory', () => {
+  it('should call in entityRechargeHistory', () => {
+    const recharge = RechargeHistoryEntity.create(fakeRecharge)
+    expect(recharge).toEqual({
+      "duration": "Dias: 0, Horas: 0, Minutos: 10",
+      "startDate": "30 de setembro de 2023 às 8:00:00 BRT",
+      "user": "Josézin",
+    })
+  })
+})
