e5bbd26f3330636bc06289a832923f8bd03d3fda
joao pedro <jjoao.monteiro15@gmail.com>
2023-09-24 01:18:32 -0300

    chore: Omit id in recharge

--- src/slices/recharges/repositories/contracts/AddRechargesRepository.ts
+++ src/slices/recharges/repositories/contracts/AddRechargesRepository.ts
@@ -1,5 +1,5 @@
 import { Recharge } from "@prisma/client";
 
 export interface AddRecharsRepository {
-  add(data: Recharge): Promise<Recharge>
+  add(data: Omit<Recharge, 'id'>): Promise<Recharge>
 }
